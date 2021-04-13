eccc08c956f844e5e7fa920d36c59636afda1f44
Kanan <kenan@luxar.se>
2021-04-13 12:04:41 +0200

    mall

---
+++ header.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8" />
+	<title>Labb 1</title>
+	<link href="css/font-awesome.css" rel="stylesheet" type="text/css" />
+	<link href="css/bootstrap.css" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri().'/bootstrap.css'; ?>">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri().'/font-awesome.css'; ?>">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri().'/style.css'; ?>">
+	<script src="js/jquery.js"></script>
+</head>
+
+
+
